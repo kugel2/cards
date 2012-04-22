7420adcf1957292cfe4259630fcb7b548b1c39f1
Florian W. <github.9.kugelfisch2@xoxy.net>
2012-04-22 12:20:46 +0200

    start

---
+++ Cards-git/src/cards/git/CardsGit.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cards.git;
+
+/**
+ *
+ * @author florian
+ */
+public class CardsGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
